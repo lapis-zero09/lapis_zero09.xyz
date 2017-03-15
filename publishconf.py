a15918cfa8d001351f8245691c45733247bc9d30
lapis-zero09 <lapis-zero09@klis.tsukuba.ac.jp>
2017-03-16 01:55:52 +0900

    Dev

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.lapis-zero09.xyz/indexp.html'
+SITEURL = 'https://www.lapis-zero09.xyz'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
